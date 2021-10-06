9526293039ab59cc290513a5433d3a1bc5b6dbe6
JOSE MIGUEL MOLINA RONDON <miguee.25@gmail.com>
2021-10-06 09:14:34 -0300

    service

--- src/main/java/cl/blackgps/back/controllers/OrdenController.java
+++ src/main/java/cl/blackgps/back/controllers/OrdenController.java
@@ -1,10 +1,6 @@
 package cl.blackgps.back.controllers;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
--- src/main/java/cl/blackgps/back/services/OrdenServiceImpl.java
+++ src/main/java/cl/blackgps/back/services/OrdenServiceImpl.java
@@ -1,12 +1,10 @@
 package cl.blackgps.back.services;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Service;
 
 import cl.blackgps.back.entities.Orden;
@@ -15,12 +13,12 @@ import cl.blackgps.back.repositories.BaseRepository;
 import cl.blackgps.back.repositories.OrdenRepository;
 
 @Service
-public class OrdenServiceImpl extends BaseServiceImpl<Orden, Integer> implements OrdenService{
-    
+public class OrdenServiceImpl extends BaseServiceImpl<Orden, Integer> implements OrdenService {
+
     @Autowired
     OrdenRepository ordenRepository;
 
-    public OrdenServiceImpl(BaseRepository<Orden, Integer> baseRepository){
+    public OrdenServiceImpl(BaseRepository<Orden, Integer> baseRepository) {
         super(baseRepository);
     }
 
@@ -28,29 +26,32 @@ public class OrdenServiceImpl extends BaseServiceImpl<Orden, Integer> implements
     @Override
     @Transactional
     public Orden saveOrden(Orden entity, List<OrdenHasEstado> ordenEstados) throws Exception {
-        try{
-
+        try {
 
             Orden newEntity = new Orden();
             newEntity = entity;
 
-            //newEntity = ordenRepository.save(entity);
+            // newEntity = ordenRepository.save(entity);
 
-            for(int i = 0; i < ordenEstados.size(); i++){
-                
-                if(ordenEstados.get(i).getOrden() == null){
+            for (int i = 0; i < ordenEstados.size(); i++) {
+
+                if (ordenEstados.get(i).getOrden() == null) {
                     System.out.println("Entré al IF");
                     ordenEstados.get(i).setOrden(newEntity);
                     System.out.println("iD ORDEN DENTRO IF: " + ordenEstados.get(i).getOrden().getIdOrden());
                 }
                 ordenEstados.get(i).setOrden(newEntity);
-                //ordenEstados.get(i).setOrdenIdOrden(newEntity.getIdOrden());
+                // ordenEstados.get(i).setOrdenIdOrden(newEntity.getIdOrden());
                 System.out.println("Orden Has Estados en SAVEORDEN");
                 System.out.println(ordenEstados.get(i).toString());
 
-                //ordenHasEstadoRepository.saveOrdenHasEstado(oe.get(i).getFechaAsignado(), oe.get(i).getIdUsuario(), oe.get(i).getEstadoIdEstado(),oe.get(i).getOrdenIdOrden());
+                // ordenHasEstadoRepository.saveOrdenHasEstado(oe.get(i).getFechaAsignado(),
+                // oe.get(i).getIdUsuario(),
+                // oe.get(i).getEstadoIdEstado(),oe.get(i).getOrdenIdOrden());
 
-                //ordenRepository.saveOrdenHasEstado(ordenEstados.get(i).getFechaAsignado(), ordenEstados.get(i).getIdUsuario(), ordenEstados.get(i).getEstadoIdEstado(),ordenEstados.get(i).getOrdenIdOrden());
+                // ordenRepository.saveOrdenHasEstado(ordenEstados.get(i).getFechaAsignado(),
+                // ordenEstados.get(i).getIdUsuario(),
+                // ordenEstados.get(i).getEstadoIdEstado(),ordenEstados.get(i).getOrdenIdOrden());
 
             }
 
@@ -61,7 +62,7 @@ public class OrdenServiceImpl extends BaseServiceImpl<Orden, Integer> implements
             newEntity = ordenRepository.save(newEntity);
 
             return newEntity;
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             throw new Exception(e.getMessage());
         }
