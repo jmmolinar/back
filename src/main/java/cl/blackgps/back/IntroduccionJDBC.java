29b59e9e674c2e74efb5b6e6519ccad36febda88
JOSE MIGUEL MOLINA RONDON <miguee.25@gmail.com>
2021-10-08 16:22:37 -0300

    no main

--- src/main/java/cl/blackgps/back/IntroduccionJDBC.java
+++ src/main/java/cl/blackgps/back/IntroduccionJDBC.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class IntroduccionJDBC {
-    public static void main(String[] args){
+    public static void mani(String[] args){
 
         SpringApplication.run(IntroduccionJDBC.class, args);
 
